b215a109e25933275ebe82e53e7ba0983ec18774
tamim <tamimhossain6421@gmail.com>
2024-01-28 18:05:18 +0600

    added navbar and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Style from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <div>
-            <p>This is footer Component </p>
+        <div className={`${Style.container}`}>
+            <footer className={`${Style.overly} footer min-h-[50vh] p-10 text-base-content`}>
+  <aside>
+    <p className='text-2xl font-bold'>Company Logo</p>
+    <p>ABC Industries Ltd.<br/>Providing reliable tech since 2022</p>
+  </aside> 
+  <nav>
+    <h6 className="footer-title">Services</h6> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <h6 className="footer-title">Company</h6> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <h6 className="footer-title">Legal</h6> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
         </div>
     );
 };
